152153833901e0db293d9e3db64324a2436f4cc0
vyvuth <vyvuth191@gmail.com>
2024-06-08 22:50:58 +0700

    add file

---
+++ test.js
@@ -0,0 +1,17 @@
+let Name = "Hello javascript";
+console.log(Name);
+
+let Age = "21";
+console.log(Age);
+
+const Apple = [1221,2525,5602];
+const a = Math.max(...Apple);
+const b = Math.min(...Apple);
+console.log(b);
+console.log(a);
+
+var firstName = "Hello world";
+console.log(firstName);
+
+var myName = "Hi java";
+console.log(myName);
